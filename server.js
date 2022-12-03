5e0419b041c2b02c52dd64c843b77fd60b9b96c9
Eilidh Ritchie <eilidh.ritchie@gmail.com>
2022-12-03 13:02:10 -0800

    JWT setup for log-in, handle profile back to frontend on login success

--- server.js
+++ server.js
@@ -2,44 +2,127 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 PORT = process.env.PORT || 5050;
-jsonSecretKey = process.env.JSONSECRETKEY;
+JWT_SECRET = process.env.JSONSECRETKEY;
+REFRESH_TOKEN = process.env.REFRESH_TOKEN_SECRET;
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 app.use(cors())
 app.use(express.json())
 
-// set up access to pages based on logged in status
-app.use((req, res, next) => {
-	//allow access to any pages not req login token
-	if (req.url === '/' || 
-			req.url === '/aboutus' || 
-			req.url === '/gallery'||
-			req.url === '/login'||
-			req.url === '/createaccount') {
-		next();
-	} else {
-		const token = getToken(req);
-		if (token) {
-			console.log('Auth Token: ', token);
-			if(jwt.verify(token, jsonSecretKey)) {
-				//decode token to pass along data to end-points that may require data from token
-				req.decode = jwt.decode(token);
-				next();
-			} else {
-				res.status(403).json({error: "Not Authorized"});
-			}
-		} else {
-			res.status(403).json({error: "No token. Unauthorized"});
-		}
+
+
+const users = [
+	{
+		username: 'eilidh',
+		password: 'test',
+		id: 1,
+		firstName: 'Eilidh',
+		lastName: 'Ritchie'
+	},
+	{
+		username: 'jim',
+		password: 'jim2',
+		id: 2,
+		firstName: 'Jim',
+		lastName: 'Ritchie'
 	}
-}) ;
+];
+
+const posts = [
+	{
+		username: 'eilidh',
+		title: 'post 1'
+	},
+	{
+		username: 'jim',
+		title: 'post 2'
+	},
+	{
+		username: 'eilidh',
+		title: 'post 3'
+	}
+]
 
 function getToken(req) {
 	return req.headers.authorization.split(" ")[1];
 }
 
-const users = {};
+// set up access to pages based on logged in status
+function checkToken(req, _res, next) {
+	const token = getToken(req);
+	if (token && jwt.verify(token, JWT_SECRET)) {
+		req.user = jwt.decode(token); //attach decoded token to req object
+		next();
+	} else {
+		res.status(401).send('No valid token sent')
+		next();
+	}
+}
+
+app.post('/login', (req, res) => {
+	if (req.body.username && req.body.password) {
+		const foundUser = users.find(
+			(user) =>
+			user.username === req.body.username && user.password === req.body.password 
+		);
+		if (foundUser) {
+			const user = {
+				id: foundUser.id, 
+				username: foundUser.username, 
+				firstName: foundUser.firstName,
+				lastName: foundUser.lastName
+			}
+			const jwtToken = jwt.sign(user ,JWT_SECRET);
+			res.json({
+				message: 'login success',
+				token: jwtToken,
+			});
+		} else {
+			res.status(401).send('This is not a valid user/password')
+		}
+	} else {
+		res.status(400).send('Please provide a username and password')
+	}
+})
+
+app.get('/posts', checkToken, (req, res) => {
+	res.json(posts.filter(post => post.username === req.user.username))
+})
+
+app.get('/profile', checkToken, (req, res) => {
+	if (req.user) {
+		res.json({
+			user: req.user
+		})
+	}
+})
+
+
+// app.use((req, res, next) => {
+// 	//allow access to any pages not req login token
+// 	if (req.url === '/' || 
+// 			req.url === '/aboutus' || 
+// 			req.url === '/gallery'||
+// 			req.url === '/login'||
+// 			req.url === '/createaccount') {
+// 		next();
+// 	} else {
+// 		const token = getToken(req);
+// 		if (token) {
+// 			console.log('Auth Token: ', token);
+// 			if(jwt.verify(token, JWT_SECRET)) {
+// 				//decode token to pass along data to end-points that may require data from token
+// 				req.decode = jwt.decode(token);
+// 				next();
+// 			} else {
+// 				res.status(403).json({error: "Not Authorized"});
+// 			}
+// 		} else {
+// 			res.status(403).json({error: "No token. Unauthorized"});
+// 		}
+// 	}
+// }) ;
 
 app.post('/createaccount', (req, res) => {
 	const { username, firstName, lastName, password } = req.body;
@@ -52,27 +135,6 @@ app.post('/createaccount', (req, res) => {
 	console.log('users object:', users);
 	res.json({ success: 'true' });
 });
-
-app.post('/login', (req, res) => {
-	const { username, password } = req.body;
-	const user = users[username];
-
-	if(user && user.password === password) {
-		console.log('Found User: ', user);
-		res.json( { token: jwt.sign({name: user.name}, jsonSecretKey)})
-	} else {
-		res.status(403).json({
-			token: '',
-			error: {
-				message: 'Error logging in. Invalid username/password combination'
-			}
-		})
-	}
-})
-
-app.get('/profile', (req, res) => {
-	res.json(req.decode);
-})
 
 
 app.listen(PORT, function () {
